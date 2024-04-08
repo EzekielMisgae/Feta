3dfb09ebb059fc86fd4196cbacf430b5f67d88ce
ezekielalene <ezkielmisgae@gmail.com>
2024-04-08 17:16:48 +0300

    Upload all files

---
+++ client/src/utils/landing_page_developers.js
@@ -0,0 +1,46 @@
+const Images = [
+    {
+        id: 0,
+        src: "/img/landing_page_images/features-01.png",
+        title: "feature1",
+    },
+    {
+        id: 1,
+        src: "/img/landing_page_images/features-02.png",
+        title: "feature2",
+    },
+    {
+        id: 2,
+        src: "/img/landing_page_images/features-03.png",
+        title: "feature3",
+    },
+    {
+        id: 3,
+        src: "/img/landing_page_images/test.jpg",
+        title: "Surafel Desalegn",
+        role: "FullStack Dev",
+        linkedinUrl: "https://www.linkedin.com/in//",
+        githubUrl: "https://github.com/",
+        websiteUrl: "https://.github.io/Protfolio-Sample/",
+    },
+    {
+        id: 4,
+        src: "/img/landing_page_images/test.jpg",
+        title: "Fiker Biset",
+        role: "FullStack Dev",
+        linkedinUrl: "https://www.linkedin.com/in//",
+        githubUrl: "https://github.com/",
+        websiteUrl: "https://.vercel.app/",
+    },
+    {
+        id: 5,
+        src: "/img/landing_page_images/test.jpg",
+        title: "Martha Anteneh",
+        role: "FullStack Dev",
+        linkedinUrl: "https://www.linkedin.com/in//",
+        githubUrl: "https://github.com/",
+        websiteUrl: "https://.vercel.app/",
+    }
+];
+
+export default Images;
