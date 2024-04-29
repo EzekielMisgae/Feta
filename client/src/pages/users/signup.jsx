c8f19213210bf2f89203c7d16b0949c236bd7cdb
ezekielalene <ezkielmisgae@gmail.com>
2024-04-29 22:20:24 +0300

    FE fix for signup page

--- client/src/pages/users/signup.jsx
+++ client/src/pages/users/signup.jsx
@@ -4,9 +4,6 @@ import { useEffect, useState } from "react";
 import { FiArrowLeft } from "react-icons/fi";
 import Cookies from "universal-cookie";
 import Link from "next/link"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
 
 
 export async function getServerSideProps(context) {
@@ -118,173 +115,124 @@ export default function Signup({ userIdCookie }) {
   }, [message]);
 
     return (
-        <div
-            className="flex min-h-[100dvh] items-center justify-center bg-gray-50 px-4 py-12 dark:bg-gray-950">
-            <div className="w-full max-w-md space-y-8">
-                <div>
-                    <h2
-                        className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-50">
-                        Create an account
-                    </h2>
-                    <p className="mt-2 text-center text-sm text-gray-600 dark:text-gray-400">
-                        Already have an account?
-                        <Link
-                            className="font-medium text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300"
-                            href="#">
-                            Sign in
-                        </Link>
+        <div>
+            <div className="m-2">
+            <FiArrowLeft
+                onClick={() => router.push("/")}
+                size={24}
+                className="cursor-pointer"
+            />
+            </div>
+            <br/><br/><br/><br/>
+            {message.errorMsg && (
+                <div className="rounded p-2 my-2 bg-red-200 text-red-600 font-medium text-center">
+                    {message.errorMsg}
+                </div>
+            )}
+            {message.successMsg && (
+                <div className="rounded p-2 my-2 bg-green-200 text-green-600 font-medium text-center">
+                    {message.successMsg}
+                </div>
+            )}
+            <br/>
+            <div class="space-y-2 text-center flex flex-col items-center justify-center ">
+                <div class="space-y-2">
+                    <h1 class="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl/none">Welcome to Feta</h1>
+                    <p class="mx-auto max-w-[600px] text-gray-500 dark:text-gray-400">
+                    The platform for discovering and ticketing your favorite events.
                     </p>
                 </div>
-                <form action="#" className="space-y-6" method="POST">
-                    <div>
-                        <Label className="sr-only" htmlFor="name">
-                            Name
-                        </Label>
-                        <div className="relative mt-1 rounded-md shadow-sm">
-                            <div
-                                className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                                <UserIcon className="h-5 w-5 text-gray-400" />
+            </div>
+            <br/>
+            <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md flex gap-8">
+                <div className="flex flex-col items-center p-4 w-1/3">
+                    <div className="w-24 h-24 rounded-full border-2 border-dashed border-gray-300 flex items-center justify-center">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="text-gray-400"
+                        >
+                            <path d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z"></path>
+                            <circle cx="12" cy="13" r="3"></circle>
+                        </svg>
+                    </div>
+                    <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 mt-4">
+                        <p>Welcome to Feta
+                        {username && (
+                            <div className="mt-2 text-gray-500">
+                                Dear, {username}!
                             </div>
-                            <Input
-                                autoComplete="name"
-                                className="block w-full rounded-md border-gray-300 pl-10 focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-50 dark:focus:border-indigo-400 dark:focus:ring-indigo-400"
-                                id="name"
-                                name="name"
-                                placeholder="Full name"
-                                required
-                                type="text" />
-                        </div>
+                        )}</p>
+                    </button>
+                    <p className="text-xs text-gray-500 mt-2"></p>
+                </div>
+                <div className="flex flex-col justify-between w-2/3 mt-8">
+                    <div className="grid grid-cols-2 gap-4">
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={email}
+                            className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                            placeholder="Email"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <button
+                            type="submit"
+                            className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2 rounded-full bg-green-400 hover:bg-green-300 text-white"
+                            onClick={handleVerifyEmail}
+                        >
+                            Verify
+                        </button>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            value={username}
+                            className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                            placeholder="Full Name"
+                            disabled={!email}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <input
+                            type="tel"
+                            id="contactNumber"
+                            name="contactNumber"
+                            value={contactNumber}
+                            className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                            placeholder="Phone Number"
+                            disabled={!email}
+                            onChange={(e) => setContactNumber(e.target.value)}
+                        />
+                        <input
+                            type="number"
+                            id="otp"
+                            name="otp"
+                            value={otp}
+                            className="flex h-10 w-full rounded-md border-none bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                            placeholder="OTP"
+                            disabled={!email}
+                            onChange={(e) => setOtp(e.target.value)}
+                        />
+                        <button
+                            type="submit"
+                            className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2 rounded-full bg-blue-400 hover:bg-blue-300 text-white"
+                            onClick={handleSubmit}
+                            disabled={!email || !otp || !contactNumber || !username}
+                        >
+                            Create Account
+                        </button>
                     </div>
-                    <div>
-                        <Label className="sr-only" htmlFor="email">
-                            Email address
-                        </Label>
-                        <div className="relative mt-1 rounded-md shadow-sm">
-                            <div
-                                className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                                <MailIcon className="h-5 w-5 text-gray-400" />
-                            </div>
-                            <Input
-                                autoComplete="email"
-                                className="block w-full rounded-md border-gray-300 pl-10 focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-50 dark:focus:border-indigo-400 dark:focus:ring-indigo-400"
-                                id="email"
-                                name="email"
-                                placeholder="Email address"
-                                required
-                                type="email" />
-                        </div>
-                    </div>
-                    <div>
-                        <Label className="sr-only" htmlFor="password">
-                            Password
-                        </Label>
-                        <div className="relative mt-1 rounded-md shadow-sm">
-                            <div
-                                className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                                <LockIcon className="h-5 w-5 text-gray-400" />
-                            </div>
-                            <Input
-                                autoComplete="new-password"
-                                className="block w-full rounded-md border-gray-300 pl-10 focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-50 dark:focus:border-indigo-400 dark:focus:ring-indigo-400"
-                                id="password"
-                                name="password"
-                                placeholder="Password"
-                                required
-                                type="password" />
-                        </div>
-                    </div>
-                    <div>
-                        <Label className="sr-only" htmlFor="confirm-password">
-                            Confirm Password
-                        </Label>
-                        <div className="relative mt-1 rounded-md shadow-sm">
-                            <div
-                                className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                                <LockIcon className="h-5 w-5 text-gray-400" />
-                            </div>
-                            <Input
-                                autoComplete="new-password"
-                                className="block w-full rounded-md border-gray-300 pl-10 focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-50 dark:focus:border-indigo-400 dark:focus:ring-indigo-400"
-                                id="confirm-password"
-                                name="confirm-password"
-                                placeholder="Confirm Password"
-                                required
-                                type="password" />
-                        </div>
-                    </div>
-                    <div>
-                        <Button
-                            className="flex w-full justify-center rounded-md border border-gray-200 border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:bg-indigo-400 dark:hover:bg-indigo-500 dark:focus:ring-indigo-400 dark:focus:ring-offset-gray-950 dark:border-gray-800"
-                            type="submit">
-                            Sign up
-                        </Button>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
-
-    );
-
-}
-
-
-function LockIcon(props) {
-    return (
-        (<svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round">
-            <rect width="18" height="11" x="3" y="11" rx="2" ry="2" />
-            <path d="M7 11V7a5 5 0 0 1 10 0v4" />
-        </svg>)
-    );
-
-}
-
-
-function MailIcon(props) {
-    return (
-        (<svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round">
-            <rect width="20" height="16" x="2" y="4" rx="2" />
-            <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
-        </svg>)
-    );
-}
-
-
-function UserIcon(props) {
-    return (
-        (<svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round">
-            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-            <circle cx="12" cy="7" r="4" />
-        </svg>)
     );
 }
 
