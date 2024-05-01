579bb51990f9c2660ede6a9e365177d0d67e5216
ezekielalene <ezkielmisgae@gmail.com>
2024-05-01 17:11:56 +0300

    Fix ui

--- client/src/pages/users/signup.jsx
+++ client/src/pages/users/signup.jsx
@@ -229,7 +229,7 @@ export default function Signup({ userIdCookie }) {
               value={username}
               className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
               placeholder="Full Name"
-              disabled={!email}
+              disabled={!email || !otp}
               onChange={(e) => setUsername(e.target.value)}
             />
             <input
@@ -239,7 +239,7 @@ export default function Signup({ userIdCookie }) {
               value={contactNumber}
               className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
               placeholder="Phone Number"
-              disabled={!email}
+              disabled={!email || !otp}
               onChange={(e) => setContactNumber(e.target.value)}
             />
             <input
