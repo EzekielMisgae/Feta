51f978377a2f6e6d2c66298d076f90b8c99c6b36
ezekielalene <ezkielmisgae@gmail.com>
2024-05-01 15:43:05 +0300

    FE edit for developers page

--- developers/src/components/header.jsx
+++ developers/src/components/header.jsx
@@ -3,12 +3,10 @@ import "../styles/style.css";
 function Header() {
     return (
         <div className="head">
-            <h1>Generate Admin Credential- Feta</h1>
+            <h1>Generate Admin Credential - Feta</h1>
             <center>
                 <p>
-                    Product owners can set admins access here. Currently in test
-                    mode so all the new admin assigned will be deleted after 24
-                    hours. You can set yourself as admin or can use the test
+                    Product owners can set admins access here. You can set yourself as admin or can use the test
                     admin credentials provided for testing purpose.
                 </p>
             </center>
