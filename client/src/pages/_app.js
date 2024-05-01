0e79e51ebd9796e09859976eb3dde9ccecb9833b
ezekielalene <ezkielmisgae@gmail.com>
2024-05-01 17:11:04 +0300

    Fix favicon on tab

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import "@/styles/Home.css";
 import Head from "next/head";
+import Image from "next/image";
 
 export default function App({ Component, pageProps }) {
     return (
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }) {
             <Head>
                 {/* <link rel="shortcut icon" href="logo" /> */}
                 <title>Feta</title>
+                <link rel="icon" href="/favicon_io/favicon.ico" />
             </Head>
             <Component {...pageProps} />
         </>
